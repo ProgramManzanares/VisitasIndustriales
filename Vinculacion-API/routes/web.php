eb71d52c3e5b0bf362467bfc11d1f6299d650783
Juan-RX <Jportizm003@gmail.com>
2024-12-18 20:22:55 -0700

    Le puse los styles correctos al panel vinculacion, checa si funciona

--- Vinculacion-API/routes/web.php
+++ Vinculacion-API/routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< Updated upstream
     return view('PanelVinculacion');
-=======
-    return view('./VistasAcademia/FormularioAcademia');
->>>>>>> Stashed changes
 });
