392d49ce329383a5aa8103079b12a8393b578d58
Juan-RX <Jportizm003@gmail.com>
2024-12-18 20:30:46 -0700

    Se agregó la ruta de css en panel vinculacion

--- Vinculacion-API/resources/views/PanelVinculacion.blade.php
+++ Vinculacion-API/resources/views/PanelVinculacion.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VINCULACION</title>
-    <link rel="stylesheet" href="styles_Panel_Vinculacion.css">
+    <link rel="stylesheet" href="{{ asset('css/styles_Panel_Vinculacion.css') }}">
+    
+    
 </head>
 <body>
     <header class="header">
