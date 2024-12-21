f42f5488efd9697fcae985dd3a5f9fb09540ef0d
Edgar Alberto Leal Duarte <tu_correo@example.com>
2024-12-20 18:17:49 -0700

    Subiendo cambios

---
+++ resources/views/VistasVinculacion/EvidenciasVisitas.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styles_Evidencias_Visitas.css">
+    <title>Evidencias de Visitas</title>
+</head>
+<body>
+    <header class="header">
+        <button class="button-header" id = "button-header">
+            <a href="javascript:history.back()" style="text-decoration: none; color: black; display: flex; align-items: center;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l3.647 3.646a.5.5 0 0 1-.708.708l-4.5-4.5a.5.5 0 0 1 0-.708l4.5-4.5a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
+                </svg>
+                <span style="margin-left: 8px;">EVIDENCIAS DE VISITAS</span>
+            </a>
+            <!--&lt Evidencias de Visitas-->
+        </button>
+    </header>
+
+    <div class="search-bar">
+        <input type="text" placeholder="Type to search...">
+        <button>🔍</button>
+    </div>
+
+    <main class="main-container">
+      <div class="container"> 
+            <div class="card">
+                <p>Evidencias de visita a empresa XYZ</p>
+                <h2>EMPRESA XYZ</h2>
+                <p>Fecha: 12/12/2024</p>
+                <button>Ver más detalles</button>
+            </div>
+            <div class="card">
+                <p>Evidencias de visita a empresa ABC</p>
+                <h2>EMPRESA ABC</h2>
+                <p>Fecha: 15/12/2024</p>
+                <button>Ver más detalles</button>
+            </div>
+            <div class="card">
+                <p>Evidencias de visita a empresa DEF</p>
+                <h2>EMPRESA DEF</h2>
+                <p>Fecha: 20/12/2024</p>
+                <button>Ver más detalles</button>
+            </div>
+            <div class="card">
+                <p>Evidencias de visita a empresa 000</p>
+                <h2>EMPRESA 000</h2>
+                <p>Fecha: 00/00/0000</p>
+                <button>Ver más detalles</button>
+            </div>
+            <div class="card">
+                <p>Evidencias de visita a empresa XYZ</p>
+                <h2>EMPRESA XYZ</h2>
+                <p>Fecha: 12/12/2024</p>
+                <button>Ver más detalles</button>
+            </div>
+            <div class="card">
+                <p>Evidencias de visita a empresa ABC</p>
+                <h2>EMPRESA ABC</h2>
+                <p>Fecha: 15/12/2024</p>
+                <button>Ver más detalles</button>
+            </div>
+            <div class="card">
+                <p>Evidencias de visita a empresa DEF</p>
+                <h2>EMPRESA DEF</h2>
+                <p>Fecha: 20/12/2024</p>
+                <button>Ver más detalles</button>
+            </div>
+            <div class="card">
+                <p>Evidencias de visita a empresa 000</p>
+                <h2>EMPRESA 000</h2>
+                <p>Fecha: 00/00/0000</p>
+                <button>Ver más detalles</button>
+            </div>
+        </div>
+    </main>
+
+    <!-- Botón de cargar más -->
+    <div class="load-more">
+        <button>Cargar más 🔄</button>
+    </div>
+</body>
+</html>
