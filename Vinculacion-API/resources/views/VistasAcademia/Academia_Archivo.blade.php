21470667b671e9bd1b988f43d1bced78e77bb74e
Juan-RX <Jportizm003@gmail.com>
2024-12-23 14:36:38 -0700

    Archivo de Academia listo

---
+++ Vinculacion-API/resources/views/VistasAcademia/Academia_Archivo.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styles_Archivo_Visitas.css">
+    <title>Archivo de Visitas</title>
+</head>
+<body>
+
+    <header class="header">
+        <button class="button-header" id = "button-header">
+            <a href="javascript:history.back()" style="text-decoration: none; color: black; display: flex; align-items: center;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l3.647 3.646a.5.5 0 0 1-.708.708l-4.5-4.5a.5.5 0 0 1 0-.708l4.5-4.5a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
+                </svg>
+                <span style="margin-left: 8px;">ARCHIVO  DE VISITAS</span>
+            </a>
+            <!--&lt Evidencias de Visitas-->
+        </button>
+    </header>
+    <main class="main-container">
+        <div class="container">
+            <table class="table">
+                <thead class="encabezado">
+                    <tr>
+                        <th>FECHA</th>
+                        <th>EMPRESA</th>
+                        <th>UBICACIÓN</th>
+                        <th>PARTICIPANTES</th>
+                        <th>DETALLES</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>12/12/2024</td>
+                        <td>Empresa XYZ</td>
+                        <td>Monterrey, NL</td>
+                        <td>35</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>15/12/2024</td>
+                        <td>Empresa ABC</td>
+                        <td>Guadalajara, JAL</td>
+                        <td>40</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>20/12/2024</td>
+                        <td>Empresa DEF</td>
+                        <td>Ciudad de México</td>
+                        <td>50</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>12/12/2024</td>
+                        <td>Empresa XYZ</td>
+                        <td>Monterrey, NL</td>
+                        <td>35</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>15/12/2024</td>
+                        <td>Empresa ABC</td>
+                        <td>Guadalajara, JAL</td>
+                        <td>40</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>20/12/2024</td>
+                        <td>Empresa DEF</td>
+                        <td>Ciudad de México</td>
+                        <td>50</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>12/12/2024</td>
+                        <td>Empresa XYZ</td>
+                        <td>Monterrey, NL</td>
+                        <td>35</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>15/12/2024</td>
+                        <td>Empresa ABC</td>
+                        <td>Guadalajara, JAL</td>
+                        <td>40</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>20/12/2024</td>
+                        <td>Empresa DEF</td>
+                        <td>Ciudad de México</td>
+                        <td>50</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>12/12/2024</td>
+                        <td>Empresa XYZ</td>
+                        <td>Monterrey, NL</td>
+                        <td>35</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>15/12/2024</td>
+                        <td>Empresa ABC</td>
+                        <td>Guadalajara, JAL</td>
+                        <td>40</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>20/12/2024</td>
+                        <td>Empresa DEF</td>
+                        <td>Ciudad de México</td>
+                        <td>50</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>12/12/2024</td>
+                        <td>Empresa XYZ</td>
+                        <td>Monterrey, NL</td>
+                        <td>35</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>15/12/2024</td>
+                        <td>Empresa ABC</td>
+                        <td>Guadalajara, JAL</td>
+                        <td>40</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>20/12/2024</td>
+                        <td>Empresa DEF</td>
+                        <td>Ciudad de México</td>
+                        <td>50</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>12/12/2024</td>
+                        <td>Empresa XYZ</td>
+                        <td>Monterrey, NL</td>
+                        <td>35</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>15/12/2024</td>
+                        <td>Empresa ABC</td>
+                        <td>Guadalajara, JAL</td>
+                        <td>40</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                    <tr>
+                        <td>20/12/2024</td>
+                        <td>Empresa DEF</td>
+                        <td>Ciudad de México</td>
+                        <td>50</td>
+                        <td><a href="#">Ver más</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
--- Vinculacion-API/routes/web.php
+++ Vinculacion-API/routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('./VistasVinculacion/InformacionEmpresas');
-=======
-    return view('./VistasAcademia/FormularioAcademia');
->>>>>>> ca127532295f3873bcc3d68c0529785bfb1b35eb
 });
