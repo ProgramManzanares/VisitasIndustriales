eb67ef2b0900436899456cf21c169ac6aff75f5c
Arath <arath.camacho@hotmail.com>
2024-12-30 12:48:37 -0700

    Mensaje de commit

---
+++ Vinculacion-API/resources/views/PanelVinculacion.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VINCULACION</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header class="header">
+        <div class="user-info">
+            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="User Photo" class="user-photo">
+            <div>
+                <h2>Juan Pérez</h2>
+                <p>Vinculacion</p>
+            </div>
+        </div>
+
+        <div class="image">
+            <img src="../images/Logo ITH 1.png" alt="Logo ith" class="header-logo">
+        </div>
+    </header>
+
+    <div class="Procesos-container">
+        <div class="Procesos">
+            <div class="icon">
+                <img src="https://cdn-icons-png.flaticon.com/512/1034/1034553.png" alt="Solicitar Visita">
+            </div>
+            <h3>Solicitar Visita</h3>
+            <p>Gestiona la solicitud de una nueva visita a la empresa.</p>
+            <a href="../Vinculacion/index.html" class="button">Solicitar</a>
+        </div>
+    
+        <div class="Procesos">
+            <div class="icon">
+                <img src="https://cdn-icons-png.flaticon.com/512/716/716784.png" alt="Ver archivo de Visita">
+            </div>
+            <h3>Ver Archivo de Visita</h3>
+            <p>Consulta los detalles de las visitas registradas.</p>
+            <a href="#" class="button">Ver archivo</a>
+        </div>
+    
+        <div class="Procesos">
+            <div class="icon">
+                <img src="https://cdn-icons-png.flaticon.com/512/2921/2921222.png" alt="Ver Evidencias de Visita">
+            </div>
+            <h3>Ver Evidencias de Visita</h3>
+            <p>Explora las fotos y documentos de las visitas realizadas.</p>
+            <a href="#" class="button">Ver evidencias</a>
+        </div>
+    
+        <div class="Procesos">
+            <div class="icon">
+                <img src="https://cdn-icons-png.flaticon.com/512/888/888021.png" alt="Información de Empresas">
+            </div>
+            <h3>Información de Empresas</h3>
+            <p>Accede a los datos de contacto y detalles de las empresas.</p>
+            <a href="#" class="button">Información</a>
+        </div>
+    
+    </div>
+</body>
+</html>
