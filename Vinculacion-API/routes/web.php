16dfc699d594146b00892524a456c6132fb0c312
Sebastian Sotelo <sebax_sr73@outlook.com>
2025-01-14 22:03:58 -0700

    Fokin Edgar

--- Vinculacion-API/routes/web.php
+++ Vinculacion-API/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('PanelVinculacion');
+    return view('.VistasAcademia/FormularioAcademia');
+
+    
+    
+
+
 });
