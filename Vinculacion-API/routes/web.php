c78d8a18108b7e6523e04d329955e3b09d4de8ab
Edgar Alberto Leal Duarte <tu_correo@example.com>
2025-02-12 13:34:14 -0700

    Cambios en EvidenciasVisitas

--- Vinculacion-API/routes/web.php
+++ Vinculacion-API/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('./VistasAcademia/FormularioAcademia');
+    return view('./VistasVinculacion/EvidenciasVisitas');
 });
